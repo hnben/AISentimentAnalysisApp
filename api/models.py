bd97e6cc05dffb2fc4a6d391cd2856aab56d1715
hnben <hnguyen10082000@gmail.com>
2024-12-12 01:55:49 -0800

    updated rest api, added db model sqlite3

--- api/models.py
+++ api/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class SentimentAnalysis(models.Model):
+    message = models.TextField(unique=True)
+    analysis_result = models.JSONField()  # or use TextField to store results as JSON string
+
+    def __str__(self):
+        return self.message
--- api/views.py
+++ api/views.py
@@ -4,6 +4,8 @@ from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+#model imports
+from transformers import AutoTokenizer, AutoModelForSequenceClassification, pipeline
 
 @api_view(['GET'])
 def get_hi(request):
@@ -17,5 +19,28 @@ def get_hi(request):
 @api_view(['POST'])
 def post_analysis(request):
     # Access the JSON payload directly
-    data = request.data
-    return Response({'message': data})
+    data = request.data 
+    message = data.get("review", "")
+    
+    #throw error if not the right format or string
+    if not isinstance(message, str):
+        return Response({'error': 'Invalid message format'}, status=400)
+    
+    
+    tokenizer = AutoTokenizer.from_pretrained("yangheng/deberta-v3-base-absa-v1.1")
+    model = AutoModelForSequenceClassification.from_pretrained("yangheng/deberta-v3-base-absa-v1.1")
+    
+    classifier = pipeline("text-classification", model=model, tokenizer=tokenizer)
+
+    # Prepare the result dictionary
+    aspect_results = {}
+
+    # Perform aspect-based sentiment analysis for each aspect
+    for aspect in ['food', 'service', 'atmosphere', 'cost']:
+        # Use the classifier on the message with the specific aspect as text_pair
+        result = classifier(message, text_pair=aspect)
+        aspect_results[aspect] = result
+
+    # Return the results as a JSON response
+    return Response({'analysis': aspect_results})
+
